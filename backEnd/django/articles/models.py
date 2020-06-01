2bc3304ae8486df4b11bdde79d07228ce139a81c
Nilesh Barla <nileshbarla@Nileshs-MacBook-Pro.local>
2020-06-01 12:35:07 +0530

    Uploading image rectified

--- backEnd/django/articles/models.py
+++ backEnd/django/articles/models.py
@@ -3,17 +3,15 @@ from django.utils import timezone
 
 from ckeditor.fields import RichTextField
 from ckeditor_uploader.fields import RichTextUploadingField
-
-from django.utils.text import slugify
+import datetime
 # Create your models here.
 
 class Article(models.Model):
     title = models.CharField(max_length=500)
     description = models.CharField(max_length=1000, default = True) 
-    image = models.ImageField(upload_to='uploads/',height_field=None, width_field=None, max_length=100, default=None)
+    image = models.ImageField(upload_to = 'uploads/', default = 'pic_folder/None/no-img.jpg')
     content = RichTextUploadingField(blank=True, null=True)
     date_time = models.DateTimeField(default = timezone.now)
-
 
     def __str__(self):
         return self.title
