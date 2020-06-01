832dd721b0ea7d8bc06080b6c8f69643ab4bf552
Nilesh Barla <nileshbarla@Nileshs-MacBook-Pro.local>
2020-06-01 22:54:27 +0530

    Added multiple cell to the backend

--- backEnd/django/articles/api/serializers.py
+++ backEnd/django/articles/api/serializers.py
@@ -5,4 +5,16 @@ from articles.models import Article
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
          model = Article
-         fields = ('id','title','description','image','date_time','content')
+         fields = (
+             'id',
+             'title',
+             'description',
+             'image',
+             'image1',
+             'image2',
+             
+             'content',
+             'content1',
+             'content2',
+
+             'date_time',)
--- backEnd/django/articles/models.py
+++ backEnd/django/articles/models.py
@@ -8,9 +8,21 @@ import datetime
 
 class Article(models.Model):
     title = models.CharField(max_length=500)
-    description = models.CharField(max_length=1000, default = True) 
-    image = models.ImageField(upload_to = 'uploads/', default = 'pic_folder/None/no-img.jpg')
+    description = models.CharField(max_length=1000, default = ' ') 
+
+    image = models.ImageField(upload_to = 'uploads/', default = 'empty', blank=True)
     content = RichTextUploadingField(blank=True, null=True)
+    
+    image1 = models.ImageField(upload_to = 'uploads/', default = 'empty1', blank=True)
+    content1 = RichTextUploadingField(blank=True, null=True)
+
+    image2 = models.ImageField(upload_to = 'uploads/', default = 'empty2', blank=False)
+    content2 = RichTextUploadingField(blank=True, null=True)
+
+    # image3 = models.ImageField(upload_to = 'uploads/', default = 'empty3', blank=True)
+    # content3 = RichTextUploadingField(blank=True, null=True)
+
+
     date_time = models.DateTimeField(default = timezone.now)
 
     def __str__(self):
